6138e20dfdd8420b85866ce90506be67ad0510e4
yongbeom.cho <unbone80@gmail.com>
2025-06-04 19:34:36 +0900

    Initial commit

---
+++ src/main/java/com/polio/poliokeycloak/PolioKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.polio.poliokeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolioKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolioKeycloakApplication.class, args);
+    }
+
+}
