5b5a6e319b334251ae58d94973d00e8b3e98aa69
yongbeom.cho <unbone80@gmail.com>
2025-06-22 16:39:16 +0900

    feat : 미사용 의존성 제거

--- src/main/java/com/polio/poliokeycloak/keycloak/helper/KeycloakHelper.java
+++ src/main/java/com/polio/poliokeycloak/keycloak/helper/KeycloakHelper.java
@@ -8,7 +8,6 @@ import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
